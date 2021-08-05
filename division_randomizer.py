55176ca66a71cdfed86fb4c50a6f0ada65885a1a
djbrauckman <djbrauckman@gmail.com>
2021-08-05 11:59:59 -0500

    Initial commit

---
+++ division_randomizer.py
@@ -0,0 +1,7 @@
+import random
+
+league_members = ['Danny','Aaron','Hahne','Geiss','Colin','Tom','Shelby','Matthew','Josh','Robert']
+random.shuffle(league_members)
+print(league_members[:5])
+print(league_members[5:])
+

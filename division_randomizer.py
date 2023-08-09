ddf677518bc1f1cb54e6cb64e03de18a30774c6c
Danny Brauckman <56003195+djbrauckman@users.noreply.github.com>
2023-08-09 09:32:57 -0500

    Update division_randomizer.py

--- division_randomizer.py
+++ division_randomizer.py
@@ -2,6 +2,9 @@ import random
 
 league_members = ['Danny','Aaron','Hahne','Geiss','Colin','Tom','Shelby','Matthew','Josh','Robert']
 random.shuffle(league_members)
-print(league_members[:5])
-print(league_members[5:])
+division_a = league_members[:5]
+division_b = league_members[5:] 
+print(division_a)
+print(division_b)
+
 
